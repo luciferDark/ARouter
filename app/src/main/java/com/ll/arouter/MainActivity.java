c2c5749ade2930ce54f6379ea72d11544246d809
kylin <342748272@qq.com>
2020-11-27 14:28:43 +0800

    1：修改构建配置搭建

--- app/src/main/java/com/ll/arouter/MainActivity.java
+++ app/src/main/java/com/ll/arouter/MainActivity.java
@@ -3,6 +3,7 @@ package com.ll.arouter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }   
+
+        log("debug" , String.valueOf(BuildConfig.debug));
+
+        if (BuildConfig.debug){
+            log("Url debug",BuildConfig.debugUrl);
+        } else {
+            log("Url release",BuildConfig.releaseUrl);
+        }
+
+    }
+
+    public void log(String... msg ){
+        if (msg == null || msg.length <= 0){
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        for (String item  :  msg){
+            buffer.append(item + "\t");
+        }
+
+        Log.d(this.getClass().getName(), "====" + buffer.toString());
+    }
 }
